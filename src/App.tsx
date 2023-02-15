1bad53086a7c9bf1322988e298a935a0e5bcfa19
Irvin Favors <irvin.favors@gmail.com>
2023-02-15 18:47:01 -0500

    Task 3 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <img src="src/images/IMG-0061.jpg" alt="Lion Logo" />
+                React Hooks and TypeScript
                 <br /> Irvin Favors
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. <br /> Hello World
+                automatically reload.
             </p>
+            <b>Hello World</b>
+            <h1>Sophmore Year Schedule</h1>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <b>Fall 2022</b>
+                        <div className="App">
+                            <div>
+                                <p>
+                                    random text because having two lists fails.
+                                </p>
+                                <div className="rectangle-Red"></div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <b>Spring 2023</b>
+                        <div className="App">
+                            <div>
+                                <ul>
+                                    <li>CISC275</li>
+                                    <li>CISC361</li>
+                                    <li>MATH210</li>
+                                    <li>POSC370</li>
+                                    <li>CRJU110</li>
+                                </ul>
+                                <div className="rectangle-Red"></div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
