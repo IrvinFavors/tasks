bc3f796ebac037e88f7f1c94137f0927fe523559
Irvin Favors <irvin.favors@gmail.com>
2023-03-15 10:36:03 -0400

    save

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,38 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+export type Holiday =
+    | "Christmas"
+    | "Thanksgiving"
+    | "Halloween"
+    | "New Year's"
+    | "Valentine's"
+    | "🎄"
+    | "🍾"
+    | "❤️"
+    | "🎃"
+    | "🦃";
+
+const CYCLE_HOLIDAY: Record<Holiday, Holiday> = {
+    "🍾": "❤️",
+    "❤️" : "🎃",
+    "🎃": "🦃",
+    "🦃": "🎄",
+    "🎄": "🍾"
+};
+
+const CYCLE_HOLIDAY_ALPHABETICALLY: Record<Holiday, Holiday> = {
+    "🎄": "🎃",
+    "🎃": "🍾",
+    "🍾": "🦃",
+    "🦃": "❤️",
+    "❤️": "🎄"
+};
+
 export function CycleHoliday(): JSX.Element {
     return <div>Cycle Holiday</div>;
+}
+
+export function CycleHolidayAlphabetically(): JSX.Element {
+    return <div>Cycle Holiday Alphabetically</div>;
 }
